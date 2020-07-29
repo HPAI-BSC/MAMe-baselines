b5acb1c442001f3cfcb836ea7d7aa15b1014a14a
Ferran Parés <ferran.pares.pont@gmail.com>
2020-07-29 16:56:28 +0200

    adding md5sum checksum to get_data.sh

--- dataset/get_data.sh
+++ dataset/get_data.sh
@@ -4,6 +4,6 @@ cd $SCRIPTPATH
 DATA_URL=https://storage.hpai.bsc.es/mame-dataset/MAMe_data.zip
 [ -e data ] && echo "There already exists a data folder. To run this code, first remove it." && exit
 echo "Donwloading..." && wget $DATA_URL &&
-echo "Checksum validation..." && echo " $(basename $DATA_URL)" | md5sum -c &&
+echo "Checksum validation..." && echo "39b11ef14dba807faf284f74ec2576d3 $(basename $DATA_URL)" | md5sum -c &&
 echo "Decompressing..." && unzip -o "$(basename $DATA_URL)" &&
 echo "Done!"
