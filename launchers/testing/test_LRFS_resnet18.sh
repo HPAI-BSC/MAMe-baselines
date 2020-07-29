790d8279f7029d7088bb73ae7d8d11457ba5ba1d
Ferran Parés <ferran.pares.pont@gmail.com>
2020-07-29 16:22:41 +0200

    fix batch sizes for testing on LR

--- launchers/testing/test_LRFS_resnet18.sh
+++ launchers/testing/test_LRFS_resnet18.sh
@@ -3,5 +3,5 @@ SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 PROJECTPATH=$(dirname $(dirname $SCRIPTPATH))
 
 PYTHONPATH=$PROJECTPATH:$PYTHONPATH \
-python baselines/trainer/test_baseline_model.py mame lrfs resnet18 1 \
+python baselines/trainer/test_baseline_model.py mame lrfs resnet18 128 \
 $PROJECTPATH/model_checkpoints/mame_lrfs_resnet18_e43.ckpt
--- launchers/testing/test_LRFS_vgg11.sh
+++ launchers/testing/test_LRFS_vgg11.sh
@@ -3,5 +3,5 @@ SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 PROJECTPATH=$(dirname $(dirname $SCRIPTPATH))
 
 PYTHONPATH=$PROJECTPATH:$PYTHONPATH \
-python baselines/trainer/test_baseline_model.py mame lrfs vgg11 1 \
+python baselines/trainer/test_baseline_model.py mame lrfs vgg11 128 \
 $PROJECTPATH/model_checkpoints/mame_lrfs_vgg11_e27.ckpt
