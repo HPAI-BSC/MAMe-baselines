96e9b956c3bea91753edc8faf533154ce23f8003
Ferran Parés <ferran.pares@bsc.es>
2021-03-03 18:14:26 +0100

    update md5sum of checkpoints

--- model_checkpoints/get_checkpoints.sh
+++ model_checkpoints/get_checkpoints.sh
@@ -3,6 +3,6 @@ SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd $SCRIPTPATH
 CKPT_URL=https://storage.hpai.bsc.es/mame-dataset/MAMe_checkpoints.zip
 echo "Donwloading..." && wget $CKPT_URL &&
-echo "Checksum validation..." && echo "5a543d45aa706fe3cd10d9cd95635cfe $(basename $CKPT_URL)" | md5sum -c &&
+echo "Checksum validation..." && echo "25794e915fc1d3f35556a84fcb1c14d6 $(basename $CKPT_URL)" | md5sum -c &&
 echo "Decompressing..." && unzip -o "$(basename $CKPT_URL)" &&
 echo "Done!"
