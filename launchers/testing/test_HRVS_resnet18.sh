bc006e12e1d7a20c68acfb4a7fb46cfcf3508359
Ferran Parés <ferran.pares@bsc.es>
2021-03-03 15:18:16 +0100

    removing outdated launchers

--- launchers/testing/test_HRVS_resnet18.sh
+++
@@ -1,8 +0,0 @@
-SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-# shellcheck disable=SC2046
-PROJECTPATH=$(dirname $(dirname $SCRIPTPATH))
-
-PYTHONPATH=$PROJECTPATH:$PYTHONPATH \
-LRU_CACHE_CAPACITY=1 \
-python baselines/trainer/test_baseline_model.py mame hrvs resnet18 1 \
-$PROJECTPATH/model_checkpoints/mame_hrvs_resnet18_e47.ckpt
--- launchers/testing/test_LRFS_resnet18.sh
+++
@@ -1,7 +0,0 @@
-SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-# shellcheck disable=SC2046
-PROJECTPATH=$(dirname $(dirname $SCRIPTPATH))
-
-PYTHONPATH=$PROJECTPATH:$PYTHONPATH \
-python baselines/trainer/test_baseline_model.py mame lrfs resnet18 128 \
-$PROJECTPATH/model_checkpoints/mame_lrfs_resnet18_e43.ckpt
